9301f7dd5f1d9634fc190b5d28acb059b780b8e7
Denny <2010@denny.me>
2010-03-01 01:12:28 +0000

    Add/edit features for news items.

--- lib/ShinyCMS/Controller/News.pm
+++ lib/ShinyCMS/Controller/News.pm
@@ -78,6 +78,55 @@ sub list_items : Chained('base') : PathPart('list-items') : Args(0) {
 }
 
 
+=head2 add_item
+
+=cut
+
+sub add_item : Chained('base') : PathPart('add') : Args(0) {
+	my ( $self, $c, $item_id ) = @_;
+	
+	# Bounce if user isn't logged in
+	unless ( $c->user_exists ) {
+		$c->stash->{ error_msg } = 'You must be logged in to add news items.';
+		$c->go('/user/login');
+	}
+	
+	# Bounce if user isn't a news admin
+	unless ( $c->user->has_role('News Admin') ) {
+		$c->stash->{ error_msg } = 'You do not have the ability to add news items.';
+		$c->response->redirect( '/news' );
+	}
+	
+	$c->stash->{ template } = 'news/edit_item.tt';
+}
+
+
+=head2 add_do
+
+=cut
+
+sub add_do : Chained('base') : PathPart('add-do') : Args(0) {
+	my ( $self, $c ) = @_;
+	
+	# Check user privs
+	die unless $c->user->has_role('News Admin');	# TODO
+	
+	# Add the item
+	my $item = $c->model('DB::NewsItem')->create({
+		author    => $c->user->id,
+		title     => $c->request->param('title'    ),
+		url_title => $c->request->param('url_title'),
+		body      => $c->request->param('body'     ),
+	});
+	
+	# Shove a confirmation message into the flash
+	$c->flash->{status_msg} = 'News item added';
+	
+	# Bounce back to the 'edit' page
+	$c->response->redirect( $c->uri_for( 'edit', $item->id ) );
+}
+
+
 =head2 edit_item
 
 =cut
@@ -85,10 +134,51 @@ sub list_items : Chained('base') : PathPart('list-items') : Args(0) {
 sub edit_item : Chained('base') : PathPart('edit') : Args(1) {
 	my ( $self, $c, $item_id ) = @_;
 	
+	# Bounce if user isn't logged in
+	unless ( $c->user_exists ) {
+		$c->stash->{ error_msg } = 'You must be logged in to edit news items.';
+		$c->go('/user/login');
+	}
+	
+	# Bounce if user isn't a news admin
+	unless ( $c->user->has_role('News Admin') ) {
+		$c->stash->{ error_msg } = 'You do not have the ability to edit news items.';
+		$c->response->redirect( '/news' );
+	}
+	
+	# Stash the news item
 	$c->stash->{ news_item } = $c->model('DB::NewsItem')->find({
 		id => $item_id,
 	});
 }
+
+
+=head2 edit_do
+
+=cut
+
+sub edit_do : Chained('base') : PathPart('edit-do') : Args(1) {
+	my ( $self, $c, $item_id ) = @_;
+	
+	# Check user privs
+	die unless $c->user->has_role('News Admin');	# TODO
+	
+	# Perform the update
+	my $item = $c->model('DB::NewsItem')->find({
+		id => $item_id,
+	})->update({
+		title     => $c->request->param('title'    ),
+		url_title => $c->request->param('url_title'),
+		body      => $c->request->param('body'     ),
+	});
+	
+	# Shove a confirmation message into the flash
+	$c->flash->{status_msg} = 'News item updated';
+	
+	# Bounce back to the 'edit' page
+	$c->response->redirect( $c->uri_for( 'edit', $item_id ) );
+}
+
 
 
 
