0f0f0ea3ea43c02f413726a730bc54b314a8e7f6
Denny de la Haye <2016@denny.me>
2016-03-01 14:14:35 +0000

    2014->2016 o.O

--- lib/ShinyCMS/Controller/PaymentHandler/PaidListSubscription/CCBill.pm
+++ lib/ShinyCMS/Controller/PaymentHandler/PaidListSubscription/CCBill.pm
@@ -197,11 +197,11 @@ sub paid_list_subscribe : Private {
 
 =head1 AUTHOR
 
-Denny de la Haye <2014@denny.me>
+Denny de la Haye <2016@denny.me>
 
 =head1 COPYRIGHT
 
-ShinyCMS is copyright (c) 2009-2014 Shiny Ideas (www.shinyideas.co.uk).
+ShinyCMS is copyright (c) 2009-2016 Shiny Ideas (www.shinyideas.co.uk).
 
 =head1 LICENSE
 
