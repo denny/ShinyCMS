dbddc1f3abde933efa4011a4b61fd6d193c870c7
Denny de la Haye <2021@denny.me>
2021-03-18 00:49:09 +0000

    WIP on tool to clone content from staging to production (etc)

---
+++ bin/dev-tools/clone-cms-template.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/env perl
+
+# ===================================================================
+# File:     bin/dev-tools/clone-cms-template.pl
+# Project:  ShinyCMS
+# Purpose:  Clone the data for a CMS Template (including its elements)
+#           from one ShinyCMS site to another, using the Duplicator.
+#
+# Author:	Denny de la Haye <2021@denny.me>
+# Copyright (c) 2009-2021 Denny de la Haye
+#
+# ShinyCMS is free software; you can redistribute it and/or modify it
+# under the terms of either the GPL 2.0 or the Artistic License 2.0
+# ===================================================================
+
+use strict;
+use warnings;
+
+use 5.010;
+
+# Set this to 1 to get more output, 0 to get less
+my $verbose = 1;
+
+# Bail out early if no template ID was provided
+my $template_id = $ARGV[0];
+exit 'Please provide the ID of the template to be cloned.' unless $template_id;
+
+# Work out where our local libs are, and load some of them
+use Cwd;
+my $shinyroot;
+my $shinylib;
+my $shinybinlib;
+BEGIN {
+    $shinyroot   =  cwd();
+    $shinyroot   =~ s{ShinyCMS/.+}{ShinyCMS};
+    $shinylib    =  $shinyroot.'/lib';
+    $shinybinlib =  $shinyroot.'/bin/lib';
+}
+use lib $shinylib;
+use lib $shinybinlib;
+
+# Helper methods for getting source schema and config details
+require 'helpers.pl';  ## no critic
+
+# Load ShinyCMS modules
+use ShinyCMS::Schema;
+use ShinyCMS::Model::Duplicator;  # Cardboard box :)
+
+
+# Get schema objects
+my $source_db = get_schema();
+my $destination_db = get_destination_schema();
+
+# Create duplicator
+my $duplicator = ShinyCMS::Model::Duplicator->new({
+	source_db      => $source_db,
+	destination_db => $destination_db,
+	verbose        => $verbose
+});
+
+# Aim...
+$duplicator->set_source_item( 'CmsTemplate', $template_id );
+
+# Fire!
+say $duplicator->clone->result;
+
+
+# Get schema object for destination database
+# (CMS will need to provide this when integrated)
+sub get_destination_schema {
+	my $config = get_config();
+
+	my $connect_info = $config->{ 'Model::Duplicator' }->{ destination_connect_info };
+
+	return ShinyCMS::Schema->connect( $connect_info );
+}
---
+++ lib/ShinyCMS/Model/Duplicator.pm
@@ -0,0 +1,222 @@
+package ShinyCMS::Model::Duplicator;
+
+use Moose;
+use MooseX::Types::Moose qw/ Bool /;
+use namespace::autoclean;
+
+use Carp;
+
+extends qw/ Catalyst::Model /;
+
+=head1 NAME
+
+ShinyCMS::Model::Duplicator
+
+=head1 DESCRIPTION
+
+Clone ShinyCMS resources between two ShinyCMS databases - including
+any essential related data (e.g. template elements with templates).
+
+=head1 SYNOPSIS
+
+	my $boink = ShinyCMS::Model::Duplicator->new({
+		source_db      => < DBIC Schema >,
+		destination_db => < DBIC Schema >,
+		verbose        => 1
+	});
+
+	$boink->source_item( $dbic_result );
+	# or
+	$boink->set_source_item( 'CmsTemplate', $template_id );
+
+	say $boink->clone->result;
+
+=cut
+
+has source_db => (
+	is      => 'ro',
+	default => undef
+);
+
+has destination_db => (
+	is      => 'ro',
+	default => undef
+);
+
+has source_item => (
+	is      => 'rw',
+	default => undef
+);
+
+has cloned_item => (
+	is      => 'rw',
+	default => undef
+);
+
+has verbose => (
+	isa     => Bool,
+	is      => 'rw',
+	default => 0,
+);
+
+has errors => (
+	is      => 'rw',
+	default => sub{ [] },
+);
+
+
+=head1 METHODS
+
+=head2 clone
+
+	my $success = $duplicator->clone;
+
+=cut
+
+sub clone {
+	my( $self ) = @_;
+
+	return $self if $self->not_ready_to_clone;
+
+	$self->create_cloned_item;
+
+	# TODO: Make this more generic
+	my @elements = $self->source_item->cms_template_elements->all;
+	$self->create_cloned_children(
+		$self->cloned_item,
+		\@elements
+	);
+
+	return $self;
+}
+
+
+=head2 set_source_item
+
+	$duplicator->set_source_item({
+		item_type => 'CmsTemplate',
+		item_id   => 123,
+	});
+
+=cut
+
+sub set_source_item {
+	my( $self, $item_type, $item_id ) = @_;
+
+ 	croak( 'set_source_item requires item_type and item_id' ) unless $item_type and $item_id;
+
+	$self->{ source_item } = $self->source_db->resultset( $item_type )->find( $item_id );
+
+	$self->add_error( $item_type .' #'. $item_id .' not found' ) unless $self->{ source_item };
+
+	return $self;
+}
+
+
+sub result {
+	my( $self ) = @_;
+
+	return $self->error_message if $self->has_errors;
+
+	return 'Duplicator cloned item to ID: '. $self->cloned_item->id ."\n";
+}
+
+
+# Private methods
+
+sub not_ready_to_clone {
+	my( $self ) = @_;
+
+	$self->add_error( 'Destination database not specified.' ) unless $self->destination_db;
+	$self->add_error( 'Source database not specified.'      ) unless $self->source_db;
+	$self->add_error( 'Source item not specified.'          ) unless $self->source_item;
+
+	return $self->has_errors;
+}
+
+
+sub create_cloned_item {
+	my( $self ) = @_;
+
+	my %source_data = $self->source_item->get_columns;
+	delete $source_data{ id };
+
+	$self->cloned_item(
+		$self->destination_db->resultset( 'CmsTemplate' )->create( \%source_data )
+	);
+
+	return $self;
+}
+
+
+# TODO: Make this more generic
+sub create_cloned_children {
+	my( $self, $cloned_item, $source_children ) = @_;
+
+	foreach my $source_child ( @$source_children ) {
+		my %source_data = $source_child->get_columns;
+		delete $source_data{ id };
+
+		$cloned_item->cms_template_elements->create( \%source_data );
+	}
+
+	return $cloned_item;
+}
+
+
+sub has_errors {
+	my( $self ) = @_;
+
+	return 1 if scalar( @{ $self->errors } ) > 0;
+	return 0;
+}
+
+sub add_error {
+	my( $self, $error_message ) = @_;
+
+	push @{$self->errors}, $error_message;
+
+	return;
+}
+
+sub error_message {
+	my( $self ) = @_;
+
+	return unless $self->has_errors;
+
+	my $error_message = "Duplicator errors:\n";
+
+	foreach my $error ( @{$self->errors} ) {
+		$error_message .= "  $error\n";
+	}
+
+	return $error_message;
+}
+
+
+
+=head1 AUTHOR
+
+Denny de la Haye <2021@denny.me>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2009-2021 Denny de la Haye.
+
+=head1 LICENSING
+
+ShinyCMS is free software; you can redistribute it and/or modify it under the terms of either:
+
+a) the GNU General Public License as published by the Free Software Foundation;
+   either version 2, or (at your option) any later version, or
+
+b) the "Artistic License"; either version 2, or (at your option) any later version.
+
+https://www.gnu.org/licenses/gpl-2.0.en.html
+https://opensource.org/licenses/Artistic-2.0
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
